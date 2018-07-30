8cdf6fb956a3eb7b01c2f87a10a021e05778df37
Erico Silva <augusto.ericosilva@gmail.com>
2018-07-30 02:53:17 -0400

    Refactor get to return HTML for text/html accept header [PoC]

--- handlers/get.js
+++ handlers/get.js
@@ -1,9 +1,6 @@
 module.exports.handler = async (event) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Got',
-            input: event
-        })
-    };
+    const body = event.headers.Accept == 'text/html' ?
+        '<h1>HELLO</h1>' :
+        JSON.stringify({message: 'Created!', event: event});
+    return {statusCode: 200, body: body};
 };
